008f52ccc4fd7bff7d0f3eeeea28c79290fd5a0b
Flemming Andersen <flemming_@hotmail.com>
2024-04-01 11:40:04 +0200

    trying again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
+//import Navbar from "./Navbar/Navbar";
 //import NavbarHook from "./NavbarHook/NavbarHook";
 import Temp from "./pages/Temp";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Join from "./pages/Join";
-import Contact from "./pages/Contact";
+//import Home from "./pages/Home";
+//import About from "./pages/About";
+//import Join from "./pages/Join";
+//import Contact from "./pages/Contact";
 
 const App = () => {
   return (
