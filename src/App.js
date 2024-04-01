08b595b5e25ba1be30c653b2aa5266a7e945eebc
Flemming Andersen <flemming_@hotmail.com>
2024-04-01 11:37:09 +0200

    Created a temp front

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 //import NavbarHook from "./NavbarHook/NavbarHook";
+import Temp from "./pages/Temp";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Join from "./pages/Join";
@@ -11,14 +12,17 @@ import Contact from "./pages/Contact";
 const App = () => {
   return (
     <Router>
-       <Navbar /> 
+       {/*<Navbar /> */}
       {/*<NavbarHook /> */}
       <main className="main-content">
         <Routes>
+          <Route path="/" element={<Temp />} />
+          {/*
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Join" element={<Join />} />
           <Route path="/Contact" element={<Contact />} />
+          */}
           {/* Define other routes that you need*/}
         </Routes>
       </main>
---
+++ src/pages/Temp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Temp.css'; // Create a CSS file for styling
+
+const Temp = () => {
+  return (
+    <div>
+        <br></br>
+        <h1 id='SpectoConsultHelp'>Coming Soon</h1>
+    </div>
+
+  );
+};
+
+export default Temp;
