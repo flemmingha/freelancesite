a333f1c4a32f512d6d543730a60751d47fee062b
flemmingha <flemming_ha@hotmail.com>
2024-06-28 13:02:13 +0200

    updates to .env

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -21,7 +21,7 @@ const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+            const baseURL = process.env.REACT_APP_API_BASE_URL;
             const response = await axios.post(`${baseURL}/api/contact`, formData, {
                 headers: {
                     'Content-Type': 'application/json'
