9dd97c7c91ad5ec2a3d3bc18044bc95ce7c4f6dc
flemmingha <flemming_ha@hotmail.com>
2024-09-06 12:14:24 +0200

    change back to previous about

---
+++ src/Components/pages/About temp.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './About.css'; // Ensure you have this CSS file for styling
+
+const About = () => {
+  return (
+    <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h1>About</h1>
+        <div id="flexContainer">
+            <div className='box2'>
+              <div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <h2>Updates Coming</h2>
+              </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default About;
